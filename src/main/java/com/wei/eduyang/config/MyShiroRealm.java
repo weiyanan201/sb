bda479c15d547ef8956be82044f41b4ed2bc1b3c
weiyanan201 <375948835@qq.com>
2018-09-06 00:29:14 +0800

    0906

--- src/main/java/com/wei/eduyang/config/MyShiroRealm.java
+++ src/main/java/com/wei/eduyang/config/MyShiroRealm.java
@@ -1,24 +1,57 @@
 package com.wei.eduyang.config;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.wei.eduyang.domain.User;
+import com.wei.eduyang.service.UserService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+import javax.annotation.Resource;
 
 /**
  * 用户认证
  */
 public class MyShiroRealm extends AuthorizingRealm {
+    @Resource
+    private UserService userService;
 
     @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        System.out.println("权限配置-->MyShiroRealm.doGetAuthorizationInfo()");
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        User userInfo = (User) principals.getPrimaryPrincipal();
+        authorizationInfo.addRole(userInfo.getUserType()+"");
+
+        return authorizationInfo;
     }
 
+    /*主要是用来进行身份认证的，也就是说验证用户输入的账号和密码是否正确。*/
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        return null;
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
+            throws AuthenticationException {
+        System.out.println("MyShiroRealm.doGetAuthenticationInfo()");
+        //获取用户的输入的账号.
+        String username = (String) token.getPrincipal();
+        System.out.println(token.getCredentials());
+        //通过username从数据库中查找 User对象，如果找到，没找到.
+        //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
+        User userInfo = userService.findUserByName(username);
+        System.out.println("----->>userInfo="+userInfo);
+        if (userInfo == null) {
+            return null;
+        }
+//        if (userInfo.getState() == 1) { //账户冻结
+//            throw new LockedAccountException();
+//        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                userInfo, //用户名
+                userInfo.getPassword(), //密码
+                getName()  //realm name
+        );
+        return authenticationInfo;
     }
+
 }
--- src/main/java/com/wei/eduyang/controller/IndexController.java
+++ src/main/java/com/wei/eduyang/controller/IndexController.java
@@ -1,9 +1,16 @@
 package com.wei.eduyang.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wei.eduyang.bean.ResultEntity;
 import com.wei.eduyang.domain.Test;
 import com.wei.eduyang.mapper.TestMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,23 +39,39 @@ public class IndexController {
 
 
     @GetMapping("login")
-    public ResultEntity login (HttpServletRequest request, HttpServletResponse response){
+    public String login (HttpServletRequest request,String userName,String password){
+        System.out.println(userName+" : "+password);
         ResultEntity resultEntity = new ResultEntity();
-        HttpSession session = request.getSession();
-        String userName = (String) session.getAttribute("userName");
-        if (StringUtils.isBlank(userName)){
-            session.setAttribute("userName","weiyanan");
-        }else{
-            System.out.println(userName);
+
+        JSONObject jsonObject = new JSONObject();
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
+        try {
+            subject.login(token);
+            jsonObject.put("token", subject.getSession().getId());
+            jsonObject.put("msg", "登录成功");
+        } catch (IncorrectCredentialsException e) {
+            jsonObject.put("msg", "密码错误");
+        } catch (LockedAccountException e) {
+            jsonObject.put("msg", "登录失败，该用户已被冻结");
+        } catch (AuthenticationException e) {
+            jsonObject.put("msg", "该用户不存在");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        resultEntity.setReturnCode(ResultEntity.SUCCESS);
-        return resultEntity;
+        return jsonObject.toJSONString();
     }
 
     @GetMapping("checkLogin")
     public ResultEntity checkLogin (HttpServletRequest request, HttpServletResponse response){
         ResultEntity resultEntity = new ResultEntity();
+
+        Subject subject = SecurityUtils.getSubject();
+        System.out.println(subject.getPrincipal());
+
         HttpSession session = request.getSession();
+        System.out.println(session.getMaxInactiveInterval());
         String userName = (String) session.getAttribute("userName");
         if (StringUtils.isBlank(userName)){
             resultEntity.setReturnCode(ResultEntity.ERROR);
@@ -56,6 +79,7 @@ public class IndexController {
             System.out.println(userName);
             resultEntity.setReturnCode(ResultEntity.SUCCESS);
         }
+        System.out.println(session.getAttribute("userName"));
 
         return resultEntity;
     }
